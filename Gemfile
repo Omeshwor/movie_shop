9ad54bf3cc5df7c70c4c7f951989201f53046c34
Omeshwor Shrestha <omeshworlalshrestha@yahoo.com>
2020-04-26 00:52:52 -0500

    Add faker gem and test categories seed

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'bootstrap-will_paginate', '~> 1.0'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+gem 'faker', '~> 2.11'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def seed_categories
+	12.times do 
+	Category.create(
+		category_name: Faker::Book.genre
+	)
+	end
+end
+
+seed_categories
